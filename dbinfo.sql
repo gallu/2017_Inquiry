fd8713184456d6a7322d0cfac981539100b2b55e
Michiaki Furusho <m.furusho+github@gmail.com>
2017-04-25 18:47:52 +0900

    2nd commit.

--- dbinfo.sql
+++ dbinfo.sql
@@ -1,11 +1,11 @@
--- �₢���킹�e�[�u��
+-- 問い合わせテーブル
 DROP TABLE IF EXISTS inquirys;
 CREATE TABLE inquirys (
-  inquiry_id INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '���j�[�N��ID',
-  email VARCHAR(320) NOT NULL COMMENT '�₢���킹�҂̃��A�h',
-  inquiry_body TEXT NOT NULL COMMENT '�₢���킹���e',
-  name VARCHAR(620) NOT NULL COMMENT '���O',
-  birthday DATE COMMENT '�a����',
+  inquiry_id INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ユニークなID',
+  email VARCHAR(320) NOT NULL COMMENT '問い合わせ者のメアド',
+  inquiry_body TEXT NOT NULL COMMENT '問い合わせ内容',
+  name VARCHAR(620) NOT NULL COMMENT '名前',
+  birthday DATE COMMENT '誕生日',
   PRIMARY KEY (`inquiry_id`)
-)CHARACTER SET 'utf8mb4', ENGINE=InnoDB, COMMENT='�P���R�[�h���u�ꌏ�̖₢���킹�v���Ӗ�����e�[�u��';
+)CHARACTER SET 'utf8mb4', ENGINE=InnoDB, COMMENT='１レコードが「一件の問い合わせ」を意味するテーブル';
 

8e7e598ddd9fcd9b4dd514a740cf66dad661d2a1
Michiaki Furusho <m.furusho+github@gmail.com>
2017-05-23 18:33:56 +0900

    smarty test

---
+++ wwwroot/smarty_test.php
@@ -0,0 +1,23 @@
+<?php
+// smarty_test.php
+
+// Smartyのinclude
+require_once( __DIR__ . '/vendor/smarty-3.1.30/libs/Smarty.class.php');
+
+// Smartyの初期設定
+$smarty_obj  =  new Smarty();
+//var_dump($smarty_obj);
+$smarty_obj->setTemplateDir( __DIR__ . '/../smarty/templates/' );
+$smarty_obj->setCompileDir( __DIR__ . '/../smarty/templates_c/' );
+
+// Smartyへのデータの入力
+$s = 'データ入力テスト';
+$smarty_obj->assign('val',  $s);
+//
+$awk['a'] = '配列のa';
+$awk['b'] = '配列のb';
+$smarty_obj->assign('ar',  $awk);
+
+// テンプレートを指定して出力
+$smarty_obj->display('smarty_test.tpl');
+
